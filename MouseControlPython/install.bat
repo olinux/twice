6587cd7e5c75096dd5d95f98ece5f66e9855f9b9
Oliver Schmid <oliver.schmid@unifr.ch>
2013-03-22 11:06:42 +0100

    remove mouse control python

--- MouseControlPython/install.bat
+++
@@ -1,5 +0,0 @@
-set path=%path%;C:\Python27
-@echo "***********************"
-@echo "Install Mod_pywebsocket"
-@echo "***********************"
-python %~dp0setup_mod_pywebsocket.py install
--- MouseControlPython/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-SCRIPT=`readlink -f $0`
-SCRIPTPATH=`dirname $SCRIPT`
-echo "***********************"
-echo "Install Mod_pywebsocket"
-echo "***********************"
-sudo python $SCRIPTPATH/setup_mod_pywebsocket.py install
